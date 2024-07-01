1b2d612a5cc3818a905d28c722e40548ebffde01
TixMartin <martin@tixintel.com>
2024-07-01 10:07:22 +0300

    Updated the game

--- components/Image.vue
+++ components/Image.vue
@@ -1,15 +1,15 @@
 <script setup>
 const props = defineProps({
-    borderColor: {
-        type: String,
-        default: null
+    modelValue: {
+      type: Number,
+      default: 0
     },
     src: String,
 })
 </script>
 
 <template>
-  <div :class="['w-60 h-60 flex items-center justify-center overflow-hidden bg-gray-200 dark:bg-gray-800 hover:cursor-pointer rounded-full', {'border-4' : borderColor, 'border-green-500' :borderColor === 'green', 'border-red-500' :borderColor === 'red'}]" @click="$emit('selected')">
+  <div :class="['w-60 h-60 flex items-center justify-center overflow-hidden bg-gray-200 dark:bg-gray-800 hover:cursor-pointer rounded-full', {'border-4' : modelValue, 'border-green-500' : modelValue > 0, 'border-red-500' : modelValue < 0}]" @click="$emit('selected')">
     <img :src="src" class="max-w-full max-h-full" alt="Image" />
   </div>
 </template>
--- pages/game.vue
+++ pages/game.vue
@@ -53,20 +53,23 @@ const items = [
 ]
 
 const initialized = ref(false)
-const goal = ref({})
-const answers = ref([{}, {}, {}, {}])
+const goal = ref(-1)
+const answers = ref(new Set())
 const round = ref(0)
 const score = ref(0)
 const answered = ref(false)
-
-function shuffleArray(array) {
-  return array.sort((a, b) => 0.5 - Math.random())
-}
+const colors = ref([])
 
 function shuffleAnswers() {
-    const shuffled = shuffleArray([...items])
-    answers.value = shuffled.slice(0, 4)
-    goal.value = answers.value[Math.floor(Math.random() * 4)]
+    answers.value.clear()
+    const cache = goal.value
+    goal.value = -1
+    while (answers.value.size < 4) {
+        let idx = Math.floor(Math.random() * items.length)
+        if (goal.value < 0 && idx !== cache) goal.value = idx
+        answers.value.add(idx)
+    }
+    console.log(answers.value)
 }
 
 onMounted(() => {
@@ -77,51 +80,32 @@ onMounted(() => {
 async function selectAnswer(answer) {
     if (answered.value) return
     answered.value = true
-    if (answers.value[answer].name === goal.value.name){
+    if (answer === goal.value) {
         score.value++
-        answers.value[answer].color = 'green'
+        colors.value[answer] = 1
     } else {
-        answers.value[answer].color = 'red'
+        colors.value[answer] = -1
     }
     round.value++
-    await new Promise(r => setTimeout(r, 2000))
-    answers.value[answer].color = null
+    await new Promise(r => setTimeout(r, 1500))
+    colors.value[answer] = 0
     shuffleAnswers()
     answered.value = false
 }
 </script>
 
 <template>
-    <table v-if="initialized">
-        <thead>
-            <tr>
-                <th colspan="2" class="text-2xl font-bold">Which image depicts a {{ goal.name }}?</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="p-2"><div>
-                    <Image :src="answers[0].image" @selected="selectAnswer(0)" :border-color="answers[0].color" />
-                </div></td>
-                <td class="p-2"><div>
-                    <Image :src="answers[1].image" @selected="selectAnswer(1)" :border-color="answers[1].color"/>
-                </div></td>
-            </tr>
-            <tr>
-                <td class="p-2"><div>
-                    <Image :src="answers[2].image" @selected="selectAnswer(2)" :border-color="answers[2].color"/>
-                </div></td>
-                <td class="p-2"><div>
-                    <Image :src="answers[3].image" @selected="selectAnswer(3)" :border-color="answers[3].color"/>
-                </div></td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="2" class="text-xl font-bold text-center">
-                    Score: {{ score }}/{{ round }}
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+    <div v-if="initialized">
+        <div class="text-2xl font-bold">
+            Which image depicts a {{ items[goal].name }}?
+        </div>
+        <div class="grid grid-cols-2 gap-2">
+            <template v-for="(item, index) in items" :key="index">
+                <Image v-if="answers.has(index)" v-model="colors[index]" :src="item.image" @selected="selectAnswer(index)"/>
+            </template>
+        </div>
+        <div class="text-xl font-bold text-center">
+            Score: {{ score }}/{{ round }}
+        </div>
+    </div>
 </template>
